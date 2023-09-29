0944bdf7b3aa71d63ae81040029fcb7140893075
Oleksandr Palinchak <41503339+PaliAlex@users.noreply.github.com>
2023-09-29 17:29:18 +0400

    6 (#8)
    6

---
+++ src/components/header/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div `
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  background-color: #c6e7f3;
+  height: 60px;
+  box-shadow: 0 2px 8px #cdcece;
+`;
+
+export const LogoWrapper = styled.div `
+    color: black;
+    font-size: 24px;
+`
